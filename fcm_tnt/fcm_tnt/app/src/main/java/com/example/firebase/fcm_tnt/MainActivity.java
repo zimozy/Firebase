faf4167133760cfe721fb75c050c77462f5b58de
Tim <eafwas@gmail.com>
2018-05-21 20:54:57 -0400

    5-21

---
+++ fcm_tnt/fcm_tnt/app/src/main/java/com/example/firebase/fcm_tnt/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.firebase.fcm_tnt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity.onCreate: ", FirebaseInstanceId.getInstance().getToken());
+    }
+}
